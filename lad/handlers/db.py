a6869cea961ce86c9276c74f766517bb02f75572
William.McGuinness <William.McGuinness@am.jll.com>
2018-10-28 12:39:30 -0500

    adding insert and query to db methods for db handler

--- lad/handlers/db.py
+++ lad/handlers/db.py
@@ -1,4 +1,6 @@
 import pyodbc
+import pandas as pd
+
 from lad.handlers.static import db_configs
 
 class DbHandler(object):
@@ -40,3 +42,65 @@ class DbHandler(object):
             self._conn.close()
             self._conn = None
 
+    def sql_to_df(self, query):
+        query = query.strip()
+        query = query.replace("\n", " ")
+        query = query.replace("\r", " ")
+        if not self._conn:
+            con = self.connect()
+        else:
+            con = self._conn
+        return pd.read_sql(query, con)
+
+    def df_to_table(self, df, table, schema, append=True):
+        con = self.connect()
+
+        if self.driver == 'SQL Server':
+            # different ways of determining if a table already exists for
+            # different drivers
+            table_exists_df = self.sql_to_df(
+                query="""
+                    SELECT *
+                    FROM INFORMATION_SCHEMA.TABLES
+                    WHERE TABLE_SCHEMA = '{0}'
+                        AND TABLE_NAME = '{1}'
+                """.format(schema, table)
+            )
+            if not table_exists_df.empty:
+                table_exists = True
+            else:
+                table_exists = False
+
+        if not table_exists:
+            #raising exception for this exercise
+            #if I were building this functionality out I would try to infer
+            # data types from the input df and create the table here
+            raise Exception('table {0}.{1} does not exist'.format(
+                schema, table
+            ))
+
+        curs = con.cursor()
+
+        if not append:
+            curs.execute(
+                """
+                DELETE FROM {0}.{1}
+                """.format(schema, table)
+            )
+
+        df_cols = ['[{}]'.format(c) for c in list(df.columns)]
+        df_cols_str = ','.join(df_cols)
+        placeholders = ','.join(['?'] * len(df_cols))
+        for index, row in df.iterrows():
+            curs.execute(
+                """
+                INSERT INTO {0}.{1}({2})
+                VALUES ({3})
+                """.format(
+                    schema, table, df_cols_str, placeholders
+                ),
+                tuple(row)
+            )
+            curs.commit()
+
+        curs.close()
