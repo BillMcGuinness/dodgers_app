1a0991efdc4f5c55ce2a2889645c2f1d98a64790
William.McGuinness <William.McGuinness@am.jll.com>
2018-10-29 09:05:34 -0500

    deleting files

--- lad/handlers/static/db_configs.py
+++
@@ -1,9 +0,0 @@
-
-LA_DB = dict(
-    dbtype='sqlserver',
-    driver='SQL Server',
-    server='USLILVMGDIM01.delphiprd.am.Joneslanglasalle.com',
-    database='GDIM_Global_DQ',
-    user=None,
-    password=None,
-)
--- lad/handlers/static/twitter_configs.py
+++
@@ -1,7 +0,0 @@
-
-LA_APP = {
-    'consumer_key': 'xvvQngUPDGTgonSz8PRjmNYVl',
-    'consumer_secret': 'C403lYsbBOk47YTKMZhgg4jG7dHvaxM3Yh2OejtiAzmJmlYjch',
-    'access_token': '362605323-N4OlAhx9mEoFuhQOQqfSKgcnYAAinN2DM9dKPBcE',
-    'access_secret': 'dBMDVGRvY61JhVDIpZ36UmelR0UBywrb0BQTMi98hcDsG'
-}
