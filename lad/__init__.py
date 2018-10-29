204226edc5ee21da327865e3d85697ae8eeb1ed4
William.McGuinness <William.McGuinness@am.jll.com>
2018-10-29 16:04:43 -0500

    building out tweet df

--- lad/__init__.py
+++ lad/__init__.py
@@ -7,3 +7,7 @@ from lad.handlers.db import (
 from lad.handlers.twitter import (
     TwitterHandler
 )
+
+from lad.xforms.twitter_xforms import (
+    get_tweet_df
+)
--- lad/xforms/twitter_xforms.py
+++ lad/xforms/twitter_xforms.py
@@ -8,4 +8,34 @@ def get_tweet_df(search_res):
         #todo something with empty res
         pass
 
-    #for tweet in tweets:
+    tweet_df = pd.DataFrame()
+
+    for tweet in tweets:
+        tweet_row = pd.DataFrame(
+            data={
+                'coordinates': [tweet['coordinates']],
+                'created_at': [tweet['created_at']],
+                'favorite_count': [tweet['favorite_count']],
+                'favorited': [tweet['favorited']],
+                # geo': [tweet['geo']],
+                'id': [tweet['id_str']],
+                # 'in_reply_to_screen_name': [tweet[
+                                  # 'in_reply_to_screen_name']],
+                # 'in_reply_to_status_id': [tweet[
+                                  # 'in_reply_to_status_id_str']],
+                # 'in_reply_to_user_id': [tweet[
+                                  # 'in_reply_to_user_id_str']],
+                'is_quote_status': [tweet['is_quote_status']],
+                'lang': [tweet['lang']],
+                # 'place': [tweet['place']],
+                'retweet_count': [tweet['coordinates']],
+                'retweeted': [tweet['retweeted']],
+                'source': [tweet['source']],
+                'text': [tweet['text']],
+                'truncated': [tweet['truncated']],
+            }
+        )
+
+        tweet_df = pd.concat([tweet_df, tweet_row])
+
+    return tweet_df
