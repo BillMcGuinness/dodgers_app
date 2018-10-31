ec385f11ad68f2aa0ca278616f7476c7629e16af
William.McGuinness <William.McGuinness@am.jll.com>
2018-10-31 16:01:02 -0500

    starting new job to pull in relevant tweets and store in db

---
+++ jobs/twitter/dodgers_twitter.py
@@ -0,0 +1,12 @@
+import lad
+
+_SEARCH_STRING = """
+    from:Dodgers OR #Dodgers OR #BleedBlue OR #Doyers OR #LADodgers
+"""
+
+def run_job():
+    pass
+
+
+if __name__ == '__main__':
+    run_job()
