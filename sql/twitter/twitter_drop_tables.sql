a58e302695d1d0c42bbe4f41a836a732a6555bb7
William.McGuinness <William.McGuinness@am.jll.com>
2018-11-02 15:35:57 -0500

    re-ordering drop tables

--- sql/twitter/twitter_drop_tables.sql
+++ sql/twitter/twitter_drop_tables.sql
@@ -1,31 +1,61 @@
-DROP TABLE dbo.searches;
-
-DROP TABLE dbo.searches_hist;
-
-DROP TABLE dbo.users;
-
-DROP TABLE dbo.users_HIST;
-
-DROP TABLE dbo.places;
-
-DROP TABLE dbo.places_HIST;
-
-DROP TABLE dbo.places_coordinates;
-
-DROP TABLE dbo.places_coordinates_HIST;
-
-DROP TABLE dbo.tweets;
-
-DROP TABLE dbo.tweets_HIST;
-
-DROP TABLE dbo.tweet_user_mention;
-
-DROP TABLE dbo.tweet_user_mention_HIST;
-
-DROP TABLE dbo.media;
-
-DROP TABLE dbo.media_HIST;
+DROP TABLE dbo.tweet_media_HIST;
 
 DROP TABLE dbo.tweet_media;
 
-DROP TABLE dbo.tweet_media_HIST;
+DROP TABLE dbo.media_HIST;
+
+DROP TABLE dbo.media;
+
+DROP TABLE dbo.tweet_user_mention_HIST;
+
+DROP TABLE dbo.tweet_user_mention;
+
+DROP TABLE dbo.tweets_HIST;
+
+DROP TABLE dbo.tweets;
+
+DROP TABLE dbo.places_coordinates_HIST;
+
+DROP TABLE dbo.places_coordinates;
+
+DROP TABLE dbo.places_HIST;
+
+DROP TABLE dbo.places;
+
+DROP TABLE dbo.users_HIST;
+
+DROP TABLE dbo.users;
+
+DROP TABLE dbo.searches_hist;
+
+DROP TABLE dbo.searches;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
